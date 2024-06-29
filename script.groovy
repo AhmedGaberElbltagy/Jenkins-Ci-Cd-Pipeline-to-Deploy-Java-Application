632fd2da80ad3df1b3b85229a669192098403c22
AhmedGaberElbltagy <ahmedelbltagy1999@gmail.com>
2024-06-29 03:48:57 +0300

    Building the image stage

--- script.groovy
+++ script.groovy
@@ -8,7 +8,7 @@ def buildImage() {
     withCredentials([usernamePassword(credentialsId: 'DockerHub_Credientials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t ahmedelbltagy/java-maven-app .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push ahmedelbltagy/java-maven-app:jma-1.0'
+        sh 'docker push ahmedelbltagy/java-maven-app'
     }
 } 
 
