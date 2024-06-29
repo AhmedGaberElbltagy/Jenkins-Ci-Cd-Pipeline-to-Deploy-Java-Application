1db8d0e55e6d5a9a1f15d1f6573ecf07ccf788b7
AhmedGaberElbltagy <ahmedelbltagy1999@gmail.com>
2024-06-29 22:25:01 +0300

    Building the image stage

--- script.groovy
+++ script.groovy
@@ -5,13 +5,21 @@ def buildJar() {
     sh 'mvn package'
 } 
 
+// def buildImage() {
+//     withCredentials([usernamePassword(credentialsId: 'DockerHub_Credientials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//         sh 'docker build -t ahmedelbltagy/java-maven-app:1.1 .'
+//         sh "echo $PASS | docker login -u $USER --password-stdin"
+//         sh 'docker push ahmedelbltagy/java-maven-app:1.1'
+//     }
+// } 
 def buildImage() {
-    withCredentials([usernamePassword(credentialsId: 'DockerHub_Credientials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t ahmedelbltagy/java-maven-app:1.1 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push ahmedelbltagy/java-maven-app:1.1'
+    withCredentials([usernamePassword(credentialsId: 'Nexus_Credientials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t 192.168.206.129:8083/repository/java-maven-app:1.0.'
+        sh "echo $PASS | docker login -u $USER --password-stdin 192.168.206.129:8083"
+        sh 'docker push 192.168.206.129:8083/repository/java-maven-app:1.0'
     }
 } 
+
 
 def deployApp() {
     echo 'deploying the application...'
