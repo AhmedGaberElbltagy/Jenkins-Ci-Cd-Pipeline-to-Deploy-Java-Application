d03160b41306cfc8bc2151feb3aee84f7368ce1e
AhmedGaberElbltagy <ahmedelbltagy1999@gmail.com>
2024-06-30 01:01:02 +0300

    Building the image stage

--- script.groovy
+++ script.groovy
@@ -13,13 +13,17 @@ def buildJar() {
 //     }
 // } 
 def buildImage() {
-    withCredentials([usernamePassword(credentialsId: 'Nexus_Credientials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t java-maven-app .'
+        
+    }
+
+def PushImage(){
+    withCredentials([usernamePassword(credentialsId: 'Nexus_Credientials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "echo $PASS | docker login -u $USER --password-stdin 192.168.206.129:8083"
         sh 'docker tag java-maven-app 192.168.206.129:8081/repository/java-maven-app'
         sh 'docker push 192.168.206.129:8083/repository/java-maven-app'
     }
-} 
+}
 
 
 def deployApp() {
